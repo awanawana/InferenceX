ab6bf8f18de9063f58b113931ee4bec227e443f2
Oseltamivir <bryansg2013@gmail.com>
2025-12-18 21:40:32 -0800

    max_num_tokens

--- benchmarks/dsr1_fp4_b200_trt_slurm.sh
+++ benchmarks/dsr1_fp4_b200_trt_slurm.sh
@@ -86,6 +86,7 @@ set -x
 
 MAX_NUM_TOKENS=$(( ($CONC+$ISL+64+63)/64*64 ))
 MAX_MODEL_LEN=$(( MAX_MODEL_LEN > 4096 ? MAX_MODEL_LEN : 4096 ))
+MAX_NUM_TOKENS=$(( MAX_NUM_TOKENS > 4096 ? MAX_NUM_TOKENS : 4096 ))
 
 # Launch TRT-LLM server
 mpirun -n 1 --oversubscribe --allow-run-as-root \
--- benchmarks/dsr1_fp8_h200_trt_slurm.sh
+++ benchmarks/dsr1_fp8_h200_trt_slurm.sh
@@ -56,6 +56,7 @@ set -x
 
 MAX_NUM_TOKENS=$(( (CONC + ISL + 64 + 63) / 64 * 64 ))
 MAX_MODEL_LEN=$(( MAX_MODEL_LEN > 4096 ? MAX_MODEL_LEN : 4096 ))
+MAX_NUM_TOKENS=$(( MAX_NUM_TOKENS > 4096 ? MAX_NUM_TOKENS : 4096 ))
 
 # Launch TRT-LLM server
 PYTHONNOUSERSITE=1 mpirun -n 1 --oversubscribe --allow-run-as-root \
